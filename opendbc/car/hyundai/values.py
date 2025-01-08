7f259fa7b30d91d29a7d20d7e7cdb3c3050b9d44
jagoff2 <taystaff.011@gmail.com>
2025-01-08 15:55:37 -0500

    s

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -31,7 +31,7 @@ class CarControllerParams:
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 250
       self.STEER_DELTA_UP = 4
-      self.STEER_DELTA_DOWN = 5
+      self.STEER_DELTA_DOWN = 7
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
     # If the max stock LKAS request is <384, add your car to this list.
@@ -44,7 +44,7 @@ class CarControllerParams:
     elif CP.flags & HyundaiFlags.ALT_LIMITS:
       self.STEER_MAX = 404
       self.STEER_DELTA_UP = 4
-      self.STEER_DELTA_DOWN = 5
+      self.STEER_DELTA_DOWN = 7
 
     # Default for most HKG
     else:
